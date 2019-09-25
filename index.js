7aff956fd88fa39c889f555b127ba29cb3614b78
EsviCodes <esther.js.vis@gmail.com>
2019-09-25 14:48:24 +0200

    Added calculateIncome() method. Tests passed

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const { Crop, Wheat, Sugarcane } = require("./crops/crops.js");
+const { Animal, Pig, Cow, Horse } = require("./animals/animals.js");
 
 class Farm {
   constructor(name) {
     this.name = name;
     this.crops = [];
+    this.animals = [];
   }
 
   addCrop(crop) {
     this.crops.push(crop);
   }
   calculateIncome() {
-    return this.crops
+    const incomeCrops = this.crops
       .map(crop => crop.getYieldInEuros())
       .reduce((a, b) => a + b, 0);
+
+    const incomeAnimals = this.animals
+      .map(animal => animal.getValueInEuros())
+      .reduce((a, b) => a + b, 0);
+
+    return incomeCrops + incomeAnimals;
+  }
+
+  addAnimal(animal) {
+    this.animals.push(animal);
   }
 }
 module.exports.Farm = Farm;
--- index.spec.js
+++ index.spec.js
@@ -1,4 +1,5 @@
-const { Crop, Wheat, Sugarcane } = require("./crops/crops.js");
+const { Wheat, Sugarcane } = require("./crops/crops.js");
+const { Pig, Cow, Horse } = require("./animals/animals.js");
 
 // TESTS
 
@@ -33,4 +34,19 @@ test("The income of a 100 Wheat + 100 Sugarcane farm is X", () => {
   farm.addCrop(new Wheat(100));
   farm.addCrop(new Sugarcane(100));
   expect(farm.calculateIncome()).toBeCloseTo(1918.37, 1);
+});
+
+// TEST Add animal to farm
+test("We can add an animal to our farm", () => {
+  const farm = new Farm("TEST_NAME");
+  farm.addAnimal(new Pig(100));
+  farm.addAnimal(new Horse(100));
+  farm.addAnimal(new Cow(100));
+});
+
+// TEST calculateIncome() Crops + Animals
+test("Animals will be taken into account for calculateIncome", () => {
+  const farm = new Farm();
+  farm.addAnimal(new Cow(100));
+  expect(farm.calculateIncome()).not.toBe(0);
 });
